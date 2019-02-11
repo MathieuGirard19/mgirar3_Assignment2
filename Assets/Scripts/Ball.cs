5677f522d2429f1f2b62dc9d323f084d05edf3d1
MathieuGirard <mathieu.m.girard@gmail.com>
2019-02-11 05:17:42 -0500

    Finished Project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+
+public class Ball : MonoBehaviour
+{   
+    //make public so we can access in insepctor
+    public float moveSpeed;
+    private Rigidbody ball;
+    private int _score;
+    public int score{
+        get{
+            return _score;
+        }
+        set{
+            _score = value;
+        }
+    }
+    public Text scoreDisplay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+        _score = 0;
+        DisplayScore();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate(){
+        //determine movement on each axis then apply force based on this input
+        float moveHorizontal = Input.GetAxis("Horizontal")*moveSpeed;
+        float moveVertical = Input.GetAxis("Vertical")*moveSpeed;
+        //AddForce is a rigid body component
+        ball.AddForce(moveHorizontal, 0f, moveVertical);
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Pickup"){
+            other.gameObject.SetActive(false);
+            score += checkColorVal(other);
+            DisplayScore();
+        }
+    }
+
+    void DisplayScore(){
+         scoreDisplay.text = "Score: " + score.ToString();
+    }
+
+    int checkColorVal(Collider other){
+        if(other.gameObject.GetComponent<Renderer>().material.color == Color.blue)
+        {
+            print("blue");
+            return 1;
+        }
+        else if(other.gameObject.GetComponent<Renderer>().material.color == Color.red)
+        {
+            print("red");
+            return 3;
+        }
+        else if(other.gameObject.GetComponent<Renderer>().material.color == Color.yellow)
+        {
+            print("yellow");
+            return 5;
+        }
+        return 0;
+    }
+
+}
---
+++ Assets/Scripts/PickUps.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUps : MonoBehaviour
+{
+    private List<GameObject> _gameObjectList;
+    private List<Color> _cubeColors;
+    private GameObject _tempObj;
+    public float radius = 10f;
+    public float rotationSpeed;
+    public Vector3 rotationAngle;
+    public GameObject prefab;
+    public int numObjects = 8;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        _cubeColors = new List<Color>();
+        _gameObjectList = new List<GameObject>();
+        _cubeColors.Add(Color.blue);
+        _cubeColors.Add(Color.red);
+        _cubeColors.Add(Color.yellow);
+        for (int i = 0; i < numObjects; i++)
+        {
+            //split circle into equal amount of angles, then pick angle using i
+            float angle = i * Mathf.PI * 2 / numObjects;
+            //set position of new object using x and z coordinates, then put to radius
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0.1f, Mathf.Sin(angle)) * radius;
+            //initiate and set to temp
+            _tempObj = Instantiate(prefab, pos, new Quaternion(90f, 45f, 45f, 0f));
+            //set the color of the object randomly from the list of colors
+            _tempObj.GetComponent<Renderer>().material.color = _cubeColors[Random.Range(0, 3)];
+            _gameObjectList.Add(_tempObj);
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //access the list and rotate each cube in list
+        foreach (GameObject temp in _gameObjectList)
+        {
+            temp.transform.Rotate(rotationAngle * rotationSpeed, Space.World);
+        }
+
+    }
+
+    
+
+    //  void OnTriggerEnter(Collider other)
+    // {
+    //     print("in");
+    //     if (other.gameObject.CompareTag("Player"))
+    //     {
+    //         print("hit");
+    //         gameObject.SetActive(false);
+    //     }
+    // }
+    
+}
